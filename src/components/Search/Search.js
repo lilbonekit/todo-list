b41a216644bd4b781f483034131cb77ea8477773
lilbonekit <mykyta.sydorenko@nure.ua>
2023-10-10 03:56:14 +0300

    ы

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -13,12 +13,12 @@ const Search = ({toDo, onPerfomSearch, setToProps}) => {
             query,
             filter
         })
-    }, [query, filter])
+    }, [query, filter, setToProps])
 
     // Выполняем фильрацию, при изменениях этих параметров
     useEffect(() => {
         onPerfomSearch(toDo, query, filter)
-    }, [toDo, filter, query])
+    }, [toDo, filter, query, onPerfomSearch ])
 
     return(
         <div className="search-panel">
